d326d783b999d0b28b40b3f0b6be2657ac014c2b
geich <eich.gabriel1@gmail.com>
2022-04-20 09:50:23 -0300

    refactor: clean signup controller

--- src/presentetion/controller/signup.ts
+++ src/presentetion/controller/signup.ts
@@ -11,19 +11,20 @@ export class SignUpController implements Controller {
 
   handler (httpRequest: HttpRequest): HttpResponse {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      const { email, password, passwordConfirmation } = httpRequest.body
 
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const requiredField of requiredFields) {
         if (!httpRequest.body[requiredField]) {
           return badRequest(new MissingParamError(requiredField))
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation is different from password'))
       }
 
-      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+      const emailIsValid = this.emailValidator.isValid(email)
 
       if (!emailIsValid) return badRequest(new InvalidParamError('email'))
     } catch (error) {
