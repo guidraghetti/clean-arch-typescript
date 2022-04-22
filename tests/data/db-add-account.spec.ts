a971e0ef499e519fc9b60a2b2bae11a0dc152c8c
geich <eich.gabriel1@gmail.com>
2022-04-22 14:14:57 -0300

    test: ensure DbAddAccount Throws if encrypter throws

--- tests/data/db-add-account.spec.ts
+++ tests/data/db-add-account.spec.ts
@@ -5,14 +5,21 @@ interface SutTypes {
   sut: DbAddAccount
   encrypterStub: Encrypter
 }
-const makeSut = (): SutTypes => {
-  class EncrypterStub {
+
+const makeEncypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
     async encrypt (value: string): Promise<string> {
       return await new Promise(resolve => resolve('hashed_password'))
     }
   }
 
   const encrypterStub = new EncrypterStub()
+
+  return encrypterStub
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncypter()
   const sut = new DbAddAccount(encrypterStub)
 
   return {
@@ -33,5 +40,19 @@ describe('DbAddAccount usecase', () => {
 
     await sut.add(accountData)
     expect(encrypterSpy).toHaveBeenCalledWith('valid_password')
+  })
+
+  test('should throw if Encrypter throws', async () => {
+    const { encrypterStub,sut } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+
+    const accountPromise = sut.add(accountData)
+
+    await expect(accountPromise).rejects.toThrow()
   })
 })
