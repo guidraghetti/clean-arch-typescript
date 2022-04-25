11fcc4878099c06502a0296f144d2531e8ea4d4c
geich <eich.gabriel1@gmail.com>
2022-04-25 15:58:18 -0300

    feat: ensure Account Mongo Repository return account on succes

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,11 +1,17 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect (url: string) {
     this.client = MongoClient.connect(url)
   },
+
   async disconnect () {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
