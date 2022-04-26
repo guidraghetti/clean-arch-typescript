4d3157b74701224a405281487d0189694ae3847b
geich <eich.gabriel1@gmail.com>
2022-04-26 12:01:32 -0300

    chore: added docker compose file for mongodb and minor refactor in jest configs

--- src/main/routes/signup.ts
+++ src/main/routes/signup.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { makeSignupController } from '../factories/signup'
+import { routerAdapter } from '../adapters/express-route-adapter'
 
 export default (router: Router): void => {
-  router.post('/signup', (_req, res) => {
-    res.send('')
-  })
+  router.post('/signup', routerAdapter(makeSignupController()))
 }
