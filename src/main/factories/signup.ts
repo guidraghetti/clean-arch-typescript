f086e28ce8e309ffee05c0c3a69ccd415e71961e
geich <eich.gabriel1@gmail.com>
2022-04-27 15:09:02 -0300

    feat: ensure signup factory call with log decorator

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,10 +1,12 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { Controller } from '../../presentetion/controller/protocols'
 import { SignUpController } from '../../presentetion/controller/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignupController = (): SignUpController => {
+export const makeSignupController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
@@ -13,5 +15,5 @@ export const makeSignupController = (): SignUpController => {
 
   const signUpController = new SignUpController(emailValidator, dbAddAccount)
 
-  return signUpController
+  return new LogControllerDecorator(signUpController)
 }
