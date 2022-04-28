6054715dafcfbd313125fc3fc0ebbd94c6d88dd6
geich <eich.gabriel1@gmail.com>
2022-04-28 12:00:34 -0300

    feat: ensure LogMongoRepository adds error log

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,13 @@
+import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError (stack: string): Promise<void> {
+    await MongoHelper
+      .getCollection('error')
+      .insertOne({
+        stack,
+        date: new Date()
+      })
+  }
+}
