01b972c37c2e8cff50a9a328df9b636331896087
geich <eich.gabriel1@gmail.com>
2022-04-29 11:49:49 -0300

    refactor: split MONGO_URL constants

--- src/main/config/constants.ts
+++ src/main/config/constants.ts
@@ -1,2 +1,3 @@
-export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/test'
+export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017'
+export const MONGO_DB = process.env.MONGO_DB || 'test'
 export const PORT = process.env.PORT || 3000
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,9 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import app from './config/app'
-import { MONGO_URL, PORT } from './config/constants'
+import { MONGO_DB, MONGO_URL, PORT } from './config/constants'
 
 MongoHelper
-  .connect(MONGO_URL)
+  .connect(`${MONGO_URL}/${MONGO_DB}`)
   .then(() => app.listen(
     PORT, () => console.log(`Server is running on http://localhost:${PORT}`
     ))
