51188e7700f4720da02726cb994801e883187cea
geich <eich.gabriel1@gmail.com>
2022-04-29 17:28:21 -0300

    test: ensure LoginController  return badrequest if no email is provided

---
+++ tests/presentetion/controllers/login.spec.ts
@@ -0,0 +1,17 @@
+import { LoginController } from '../../../src/presentetion/controller/login/login'
+import { MissingParamError } from '../../../src/presentetion/errors'
+import { badRequest } from '../../../src/presentetion/helpers/http-helper'
+
+describe('Login Controller', () => {
+  test('should should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
