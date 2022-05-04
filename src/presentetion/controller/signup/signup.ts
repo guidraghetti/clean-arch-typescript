46925ca34c97b026459ac8d59bf590c49050680e
geich <eich.gabriel1@gmail.com>
2022-05-04 11:42:52 -0300

    refactor: error log removed from SignupContropper

--- src/presentetion/controller/signup/signup.ts
+++ src/presentetion/controller/signup/signup.ts
@@ -41,7 +41,6 @@ export class SignUpController implements Controller {
 
       return success(account)
     } catch (error) {
-      console.error(error)
       return serverError(error)
     }
   }
