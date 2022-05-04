e1c4d7f565fb2982535a0e26b930e75fbc2639ae
geich <eich.gabriel1@gmail.com>
2022-05-04 11:06:54 -0300

    feat: ensure SignupController calls with Validation helper

--- src/presentetion/controller/signup/signup.ts
+++ src/presentetion/controller/signup/signup.ts
@@ -1,21 +1,24 @@
 import { AddAccount } from '../../../domain/usecases/add-account'
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest, serverError, success } from '../../helpers/http-helper'
+import { Validation } from '../../helpers/validators/validation'
 import { EmailValidator, Controller, HttpRequest, HttpResponse } from './signup-protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
+  private readonly validation: Validation
 
-  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount, validation: Validation) {
     this.emailValidator = emailValidator
     this.addAccount = addAccount
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const { email, name, password, passwordConfirmation } = httpRequest.body
-
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const requiredField of requiredFields) {
         if (!httpRequest.body[requiredField]) {
@@ -39,6 +42,7 @@ export class SignUpController implements Controller {
 
       return success(account)
     } catch (error) {
+      console.error(error)
       return serverError(error)
     }
   }
