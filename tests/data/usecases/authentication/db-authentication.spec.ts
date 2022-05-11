eddd0c00688f78e49212ebf81f9be55381ad120e
geich <eich.gabriel1@gmail.com>
2022-05-11 15:43:15 -0300

    refacitr: rename LoadAccountByEmailRepository load to loadByEmail

--- tests/data/usecases/authentication/db-authentication.spec.ts
+++ tests/data/usecases/authentication/db-authentication.spec.ts
@@ -20,7 +20,7 @@ const makeFakeAuthenticantion = (): AuthenticationModel => ({
 
 const makeLoadAccountByEmailRepositoryStub = (): LoadAccountByEmailRepository => {
   class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
-    async load (email: string): Promise<AccountModel> {
+    async loadByEmail (email: string): Promise<AccountModel> {
       const account = makeFakeRequest()
 
       return account
@@ -87,7 +87,7 @@ describe('DB Authentication Usecase', () => {
   test('Should call LoadAccountByEmailRepository with correct email', async () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
 
-    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail')
     await sut.auth(makeFakeAuthenticantion())
 
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
@@ -96,7 +96,7 @@ describe('DB Authentication Usecase', () => {
   test('Should throw if LoadAccountByEmailRepository throws', async () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
 
-    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(Promise.reject(new Error()))
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(Promise.reject(new Error()))
 
     const authPromise = sut.auth(makeFakeAuthenticantion())
 
@@ -106,7 +106,7 @@ describe('DB Authentication Usecase', () => {
   test('Should return null if LoadAccountByEmailRepository return null', async () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
 
-    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(null)
 
     const accessToken = await sut.auth(makeFakeAuthenticantion())
 
