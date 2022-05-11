bd2b6c71270df0b5f018f4d21f0c6ba98c1149f1
geich <eich.gabriel1@gmail.com>
2022-05-11 15:34:04 -0300

    test: ensure JwtAdapter sign trhow if sign throws

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -27,4 +27,16 @@ describe('Jwt Adapter', () => {
 
     expect(token).toBe('any_token')
   })
+
+  test('should throw if sign throws', async () => {
+    const sut = makeSut()
+
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.encrypt('any_id')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
