a8b17390baecec3f6c3946e1f864fe36051623ec
geich <eich.gabriel1@gmail.com>
2022-05-12 14:55:32 -0300

    test: ensure login route return 401 if wrong credentials are provided

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,29 +1,51 @@
+import { hash } from 'bcryptjs'
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../../src/infra/db/mongodb/helpers/mongo-helper'
 import app from '../../../src/main/config/app'
 
+let accountCollection: Collection
+let password: string
+
 describe('Login routes', () => {
+  beforeAll(async () => {
+    password = await hash('any_password', 12)
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await accountCollection.deleteMany({})
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+  })
+
   describe('Signup Routes', () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(process.env.MONGO_URL)
-    })
-
-    afterAll(async () => {
-      await MongoHelper.disconnect()
-    })
-
-    beforeEach(async () => {
-      const accountCollection = MongoHelper.getCollection('accounts')
-      await accountCollection.deleteMany({})
-    })
-
     test('should return a 200 response', async () => {
       await request(app).post('/signup').send({
-        name: 'Gabriel',
-        email: 'gabriel@gmail.com',
-        password: '123456',
-        passwordConfirmation: '123456'
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: password,
+        passwordConfirmation: password
       }).expect(200)
+    })
+  })
+
+  describe('Login Routes', () => {
+    test('should return 200 on login', async () => {
+      await request(app).post('/login').send({
+        email: 'any_email@mail.com',
+        password: password
+      }).expect(200)
+    })
+
+    test('should return 401 on login with wrong credentials', async () => {
+      await request(app).post('/login').send({
+        email: 'wrong_email@mail.com',
+        password: 'wrong_gpassword'
+      }).expect(401)
     })
   })
 })
