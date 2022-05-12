640cf7960c92ee93fea5de8dcebe220b28549709
geich <eich.gabriel1@gmail.com>
2022-05-12 14:14:39 -0300

    refactor: rename signup to login-routes

---
+++ tests/main/routes/login-routes.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import { MongoHelper } from '../../../src/infra/db/mongodb/helpers/mongo-helper'
+import app from '../../../src/main/config/app'
+
+describe('Login routes', () => {
+  describe('Signup Routes', () => {
+    beforeAll(async () => {
+      await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+      await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+      const accountCollection = MongoHelper.getCollection('accounts')
+      await accountCollection.deleteMany({})
+    })
+
+    test('should return a 200 response', async () => {
+      await request(app).post('/signup').send({
+        name: 'Gabriel',
+        email: 'gabriel@gmail.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }).expect(200)
+    })
+  })
+})
