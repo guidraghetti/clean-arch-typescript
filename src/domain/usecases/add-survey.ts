95ac375ca67157892261a3b4f355026264e0e899
geich <eich.gabriel1@gmail.com>
2022-05-23 15:04:21 -0300

    feat: ensure AddSurveyController calls addSurvey.add

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,13 @@
+type SurveyAnsware = {
+  image: string
+  answer: string
+}
+
+export interface AddSurveyModel {
+  question: string
+  answers: SurveyAnsware[]
+}
+
+export interface AddSurvey {
+  add: (survey: AddSurveyModel) => Promise<void>
+}
--- src/presentation/controller/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controller/survey/add-survey/add-survey-controller.ts
@@ -1,9 +1,11 @@
+import { AddSurvey } from '../../../../domain/usecases/add-survey'
 import { badRequest } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validation } from '../../protocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -12,6 +14,11 @@ export class AddSurveyController implements Controller {
     if (error) {
       return badRequest(error)
     }
+
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({
+      question, answers
+    })
 
     return null
   }
