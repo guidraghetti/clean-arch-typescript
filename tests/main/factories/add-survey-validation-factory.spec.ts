610b339f9cb3e4b5f1cb8ca42097a8ea39370301
geich <eich.gabriel1@gmail.com>
2022-05-24 16:44:31 -0300

    test: ensure survey route run with unit and integration tests

---
+++ tests/main/factories/add-survey-validation-factory.spec.ts
@@ -0,0 +1,19 @@
+import { makeAddSurveyValidationFactory } from '../../../src/main/factories/controllers/survey/add-survey/add-survey-validation-factory'
+import { Validation } from '../../../src/presentation/controller/protocols'
+import { ValidationComposite, ValidationRequiredField } from '../../../src/validation/validators'
+
+jest.mock('../../../src/validation/validators/validation-composite')
+
+describe('Add Survey Validation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidationFactory()
+
+    const validations: Validation[] = []
+
+    for (const field of ['question', 'answers']) {
+      validations.push(new ValidationRequiredField(field))
+    }
+
+    expect(ValidationComposite).toHaveBeenLastCalledWith(validations)
+  })
+})
---
+++ tests/main/routes/survey-routes.test.ts
@@ -0,0 +1,33 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../../../src/infra/db'
+import request from 'supertest'
+import app from '../../../src/main/config/app'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await surveyCollection.deleteMany({})
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = MongoHelper.getCollection('accounts')
+  })
+
+  describe('POST /surveys', () => {
+    test('should return 204 on success', async () => {
+      await request(app).post('/surveys').send({
+        question: 'any_question',
+        answers: [{
+          answer: 'any_answer',
+          image: 'http://image.com/image.jpg'
+        }]
+      }).expect(204)
+    })
+  })
+})
