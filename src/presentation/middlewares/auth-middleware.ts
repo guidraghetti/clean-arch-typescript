ec168321286eab6a1f9a98e560cc7e6f2a59075e
geich <eich.gabriel1@gmail.com>
2022-05-26 15:56:43 -0300

    feat: ensure auth middleware call LoadAccountByToken

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,20 @@
+import { LoadAccountByToken } from '../../domain/usecases'
 import { AccessDeniedError } from '../errors'
 import { forbidden } from '../helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols'
 
 export class AuthMiddleware {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = httpRequest.headers?.['x-access-token']
+
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken)
+    }
+
     return forbidden(new AccessDeniedError())
   }
 }
