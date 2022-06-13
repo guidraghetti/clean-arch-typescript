7fd1fd2e854f7a3a5f64099a0d97cca1407b4687
geich <eich.gabriel1@gmail.com>
2022-06-13 16:07:27 -0300

    refactor: move auth midddlewares to a file

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express'
-import { adaptMiddleware, routerAdapter } from '../adapters'
+import { routerAdapter } from '../adapters'
 import { makeAddSurveyControllerFactory } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { makeLoadSurveysController } from '../factories/controllers/survey/load-survey/load-surveys-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { adminAuth, auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const auth = adaptMiddleware(makeAuthMiddleware())
-
   router.post('/surveys', adminAuth, routerAdapter(makeAddSurveyControllerFactory()))
   router.get('/surveys', auth, routerAdapter(makeLoadSurveysController()))
 }
