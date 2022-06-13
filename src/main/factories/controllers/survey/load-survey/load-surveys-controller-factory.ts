979967dd1b89762425c0f2d55b942529ad812753
geich <eich.gabriel1@gmail.com>
2022-06-13 14:39:20 -0300

    feat: add Survey Route

---
+++ src/main/factories/controllers/survey/load-survey/load-surveys-controller-factory.ts
@@ -0,0 +1,10 @@
+import { LoadSurveysController } from '../../../../../presentation/controller/survey/load-survey/load-surveys-controller'
+import { Controller } from '../../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveys } from '../../../usecases/survey/load-survey/db-load-surves-factory'
+
+export const makeLoadSurveysController = (): Controller => {
+  const controller = new LoadSurveysController(makeDbLoadSurveys())
+
+  return makeLogControllerDecorator(controller)
+}
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { adaptMiddleware, routerAdapter } from '../adapters'
 import { makeAddSurveyControllerFactory } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-survey/load-surveys-controller-factory'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleware())
 
   router.post('/surveys', adminAuth, routerAdapter(makeAddSurveyControllerFactory()))
+  router.get('/surveys', auth, routerAdapter(makeLoadSurveysController()))
 }
