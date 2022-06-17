4584bd1428f240ebb3e52945568b0d2af7d0f892
geich <eich.gabriel1@gmail.com>
2022-06-17 16:25:14 -0300

    fix: remove .test and .map from route mapper

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,7 +6,7 @@ export const setupRoutes = (app: Express): void => {
   const router = Router()
   app.use('/', router)
 
-  readdirSync(join(__dirname, '../routes')).forEach(async routeFile =>
-    (await import(`../routes/${routeFile}`)).default(router)
-  )
+  readdirSync(join(__dirname, '../routes')).forEach(async routeFile => {
+    if (!routeFile.includes('.test.') && !routeFile.includes('.map')) (await import(`../routes/${routeFile}`)).default(router)
+  })
 }
