593bcf04d3a2a891b2c917681911e8eacc1d8466
geich <eich.gabriel1@gmail.com>
2022-06-21 17:19:03 -0300

    feat: add DbSaveSurvey usecase

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -0,0 +1,10 @@
+import { SaveSurveyResult, SaveSurveyResultModel } from '@/domain/usecases'
+import { SurveyResultModel } from '../../../domain/models/survey-result'
+import { SaveSurveyResultRepository } from '../../protocols'
+
+export class DbSaveSurveyResult implements SaveSurveyResult {
+  constructor (private readonly saveSurveyResultRepository: SaveSurveyResultRepository) {}
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    return this.saveSurveyResultRepository.save(data)
+  }
+}
