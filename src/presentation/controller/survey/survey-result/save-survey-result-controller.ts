93fe5a10677f9215cdba596a8087ffafdaa8c4b1
geich <eich.gabriel1@gmail.com>
2022-06-24 11:29:20 -0300

    feat: add SaveSurveyResultController

---
+++ src/presentation/controller/survey/survey-result/save-survey-result-controller.ts
@@ -0,0 +1,11 @@
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { LoadSurveyById } from '../../../../domain/usecases'
+
+export class SaveSurveyResultController implements Controller {
+  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    return null
+  }
+}
