b8e880e35b778d8e03d698e8f72b9e03c2dbe5d6
geich <eich.gabriel1@gmail.com>
2022-06-24 13:45:02 -0300

    feat: ensure SaveSurveyRestultController call SaveSurveyResult with correct values

--- src/presentation/controller/survey/survey-result/save-survey-result-controller.ts
+++ src/presentation/controller/survey/survey-result/save-survey-result-controller.ts
@@ -1,15 +1,16 @@
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
-import { LoadSurveyById } from '../../../../domain/usecases'
+import { LoadSurveyById, SaveSurveyResult } from '../../../../domain/usecases'
 import { InvalidParamError } from '../../../errors'
 import { forbidden, serverError } from '../../../helpers/http/http-helper'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (private readonly loadSurveyById: LoadSurveyById, private readonly saveSurveyResult: SaveSurveyResult) {}
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle ({ body, params, accountId }: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-      const { answer } = httpRequest.body
+      const { answer } = body
+      const { surveyId } = params
+      const survey = await this.loadSurveyById.loadById(surveyId)
 
       if (survey) {
         const answers = survey.answers.map(answer => answer.answer)
@@ -20,6 +21,15 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
+
+      await this.saveSurveyResult.save({
+        accountId,
+        surveyId,
+        answer,
+        createdAt: new Date()
+      })
+
+      return null
     } catch (error) {
       return serverError(error)
     }
--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -7,4 +7,5 @@ export type HttpRequest = {
   headers?: any
   body?: any
   params?: Record<string, string>
+  accountId?: string
 }
