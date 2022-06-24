11de9b7c525c524a96dcb377154289a382eaf627
geich <eich.gabriel1@gmail.com>
2022-06-24 11:34:43 -0300

    feat: return forbidden if invalid surveyId is provided

--- src/presentation/controller/survey/survey-result/save-survey-result-controller.ts
+++ src/presentation/controller/survey/survey-result/save-survey-result-controller.ts
@@ -1,11 +1,18 @@
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { LoadSurveyById } from '../../../../domain/usecases'
+import { InvalidParamError } from '../../../errors'
+import { forbidden } from '../../../helpers/http/http-helper'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
+
     return null
   }
 }
