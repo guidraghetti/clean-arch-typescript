478654e86771421a9e7a725e302e7902cd5bd3b9
geich <eich.gabriel1@gmail.com>
2022-07-06 16:06:01 -0300

    test: ensure Survey Routes should return 403 on save survey without accessToken

--- tests/main/routes/survey-routes.test.ts
+++ tests/main/routes/survey-routes.test.ts
@@ -91,4 +91,12 @@ describe('Survey Routes', () => {
         .expect(200)
     })
   })
+
+  describe('PUT /sureveys/:survey_id/results', () => {
+    test('should return 403 on save survey without accessToken', async () => {
+      await request(app).put('/surveys/any_id/results').send({
+        answer: 'any_answer'
+      }).expect(403)
+    })
+  })
 })
