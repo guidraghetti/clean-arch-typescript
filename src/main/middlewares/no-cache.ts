d3c55e6f31071ab726f767f3d746c3019dd32636
geich <eich.gabriel1@gmail.com>
2022-07-14 16:32:31 -0300

    feat: add no cache middleware

---
+++ src/main/middlewares/no-cache.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const noCache = (_req: Request, res: Response, next: NextFunction): void => {
+  res.header('cache-control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  res.header('pragma', 'no-cache')
+  res.header('expires', '0')
+  res.header('surrogate-control', 'no-store')
+  next()
+}
