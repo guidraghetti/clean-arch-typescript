d0dd30d133d6862a6536ab2c63e704614a429a95
geich <eich.gabriel1@gmail.com>
2022-07-14 16:34:58 -0300

    refactor: add no cache middleware in swagger

--- src/main/config/swagger-config.ts
+++ src/main/config/swagger-config.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express'
 import { serve, setup } from 'swagger-ui-express'
 import { swaggerConfig } from '../docs'
+import { noCache } from '../middlewares/no-cache'
 
 export const setupSwagger = (app: Express): void => {
-  app.use('/docs', serve, setup(swaggerConfig))
+  app.use('/docs', noCache, serve, setup(swaggerConfig))
 }
