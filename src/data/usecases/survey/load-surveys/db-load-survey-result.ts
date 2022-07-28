2ab1e0aff4957025dac5abd9354265f1a0ff303b
geich <eich.gabriel1@gmail.com>
2022-07-28 11:46:25 -0300

    refactor: ensure DbLoadSurveyResult call loadSurveyByIdRepository if loadSurveyResultRepository returns null

--- src/data/usecases/survey/load-surveys/db-load-survey-result.ts
+++ src/data/usecases/survey/load-surveys/db-load-survey-result.ts
@@ -1,10 +1,36 @@
 import { LoadSurveyResult } from '@/domain/usecases/survey/load-survey-result'
 import { SurveyResultModel } from '@/domain/models/survey-result'
-import { LoadSurveyResultRepository } from '@/data/protocols'
+import { LoadSurveyByIdRepository, LoadSurveyResultRepository } from '@/data/protocols'
+import { SurveyModel } from '../../../../domain/models/survey'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+  constructor (
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {}
+
+  private makeEmptyResult (survey: SurveyModel): SurveyResultModel {
+    return {
+      surveyId: survey.id,
+      question: survey.question,
+      createdAt: survey.createdAt,
+      answers: survey.answers.map(answer => ({
+        ...answer,
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }))
+    }
+  }
+
   async load (surveyId: string): Promise<SurveyResultModel> {
-    return this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+
+    if (!surveyResult) {
+      const survey = await this.loadSurveyByIdRepository.loadById(surveyId)
+      surveyResult = this.makeEmptyResult(survey)
+    }
+
+    return surveyResult
   }
 }
