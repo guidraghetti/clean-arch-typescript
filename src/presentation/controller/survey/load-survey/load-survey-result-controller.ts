dcca117c83eca764b1de9213f81bd33e664b19ab
geich <eich.gabriel1@gmail.com>
2022-07-28 15:18:57 -0300

    feat: add loadSurveyResult in LoadSurveyResultController

--- src/presentation/controller/survey/load-survey/load-survey-result-controller.ts
+++ src/presentation/controller/survey/load-survey/load-survey-result-controller.ts
@@ -1,18 +1,21 @@
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
-import { LoadSurveyById } from '@/domain/usecases'
+import { LoadSurveyById, LoadSurveyResult } from '@/domain/usecases'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class LoadSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (private readonly loadSurveyById: LoadSurveyById, private readonly loadSurveyResult: LoadSurveyResult) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      const { surveyId } = httpRequest.params
+      const survey = await this.loadSurveyById.loadById(surveyId)
 
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
+
+      await this.loadSurveyResult.load(surveyId)
       return null
     } catch (error) {
       return serverError(error)
